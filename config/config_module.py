a34e4179be58623615d43974ca5ff62f1cc9ea55
LorezZ <90155169+LorezZ1000@users.noreply.github.com>
2024-06-17 20:00:26 +0000

    rchan ver 1.3

--- config/config_module.py
+++ config/config_module.py
@@ -4,4 +4,4 @@ from database_modules import database_module
 def check_banned_user():
     session["user_ip"] = request.headers.get('CF-Connecting-IP') or request.headers.get('X-Forwarded-For')
     if database_module.check_banned_user(session["user_ip"]):
-        return render_template('banned.html')
+        return render_template('utils/banned.html')
